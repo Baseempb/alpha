7ff0ac014d343c526e21445ffbba3707de75b073
Danwand <45515141+DanBrown47@users.noreply.github.com>
2020-06-09 14:38:35 +0530

    Out in string Format for further DEV

--- alpha.py
+++ alpha.py
@@ -7,6 +7,7 @@ upper_alphabet = string.ascii_uppercase
 lower_alphabet = string.ascii_lowercase
 special_character = ['!', '@', '$', '&', '*', ':', ';', '.', '?']
 
+pass_list = []
 
 def generate(password_length):
     for i in range(password_length):
@@ -15,14 +16,18 @@ def generate(password_length):
         comp_3 = randint(0, 9)
         comp_4 = random.choice(special_character)
         value = str(random.choice([comp_1, comp_2, comp_3, comp_4]))
-        print(value, end='')
-        
-
+        # print(value, end='')
+        pass_list.append(value)
+    password = ''.join([str(elem) for elem in pass_list])
+    return password
 
 password_length = int(input(f'\nNB:-minimum length is 8\nEnter password length: '))
 if password_length >= 8:
-    generate(password_length)
+    pass0 = generate(password_length)
+    print(f"The generated password is \n {pass0} \t : \t ")
 else:
     print('Enter minimum length 8')
+
+
 
 
